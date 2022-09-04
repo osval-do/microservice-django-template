4591d5a8ad86552a259447f7d60e237a4445b7e9
aldo-k <19661377+aldo-k@users.noreply.github.com>
2022-09-04 10:33:37 -0600

    example test

--- backend/tests.py
+++ backend/tests.py
@@ -1,4 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
+class ExampleTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="user_test")
+
+    def test_create_user(self):
+        test_created_user = User.objects.get(username="user_test")
+        self.assertIsNotNone(test_created_user)
 
 # Create your tests here.
+
+
